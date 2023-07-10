a66f8ea76a0a1992880dcb465b539d5ef635026d
Matias Cancina <cancinamatias@gmail.com>
2023-07-10 18:20:30 -0300

    elimina imagen de la validacion para que se ponga una default si no se agrega una

--- api/src/middlewares/validateDogMiddleware.js
+++ api/src/middlewares/validateDogMiddleware.js
@@ -1,6 +1,6 @@
 const validateDogMiddleware = async (req, res, next) => {
-    const { name, height, weight, life_span, image, temperaments } = req.body;
-    if (!name || !life_span || !image || !height || !weight || !temperaments)
+    const { name, height, weight, life_span, temperaments } = req.body;
+    if (!name || !life_span || !height || !weight || !temperaments)
         return res.status(400).json({ message: 'Missing values' })
     next()
 }
