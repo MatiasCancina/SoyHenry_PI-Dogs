cd799f7f90570e9b4530bfcca78f2381d06c7b0e
Matias Cancina <cancinamatias@gmail.com>
2023-07-11 19:08:54 -0300

    agrega loader

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,18 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 import style from './Card.module.css';
 
 export const Card = ({ dog }) => {
-    const tempResult = dog.temperament
+    const navigate = useNavigate()
+
+    const navigateHandler = () => {
+        navigate(`/details/${dog.id}`);
+    }
+
+    const apiAndDbTemps = dog.temperament
         ? dog.temperament
-        : (dog.temperaments
-            && dog.temperaments)
+        : (
+            dog.temperaments
+            && dog.temperaments
+        )
 
     return (
         <div className={style.cardContainer} key={dog.id}>
             <h1>{dog.name}</h1>
-            <img className={style.image} src={dog.image} alt={dog.name} />
+            <img onClick={navigateHandler} className={style.image} src={dog.image} alt={dog.name} />
             <div className={style.dogInfo}>
                 <p>Weight: {dog.weight}</p>
-                <p>Temperaments: {tempResult}</p>
+                <p>Temperaments: {apiAndDbTemps}</p>
             </div>
         </div>
     )
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,18 +1,18 @@
 import { Card } from '../Card/Card';
+import Loader from '../Loader/Loader';
 import style from './Cards.module.css';
 
 const Cards = ({dogs}) => {
 
     return (
         <div>
-
             <div className={style.cardsContainer}>
                 {
                     dogs.length
                         ? dogs?.map(dog =>
                             <Card key={dog.id} dog={dog} />
                         )
-                        : <p>LOADING</p>
+                        : <Loader/>
                 }
             </div>
         </div>
