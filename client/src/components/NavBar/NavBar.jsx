a4ad54fd28611e33bd46c16b3fb9a06cca03835d
Matias Cancina <cancinamatias@gmail.com>
2023-07-12 14:48:15 -0300

    renderizado condicional de searchbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,16 @@
 import SearchBar from "../SearchBar/SearchBar";
 import style from './NavBar.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+    const location = useLocation();
+
+
+
     return (
         <div className={style.navbarContainer}>
             <NavLink className={style.navLink} to='/home'>Happy Paws</NavLink>
-            <SearchBar />
+            {location.pathname === '/home' && <SearchBar />}
         </div>
     )
 }
