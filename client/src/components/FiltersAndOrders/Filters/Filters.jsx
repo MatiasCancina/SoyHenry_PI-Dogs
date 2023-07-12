eba9da18e0a1ef41be36c8d56a3d947ecd544171
Matias Cancina <cancinamatias@gmail.com>
2023-07-12 18:35:18 -0300

    crea filterByTemps

--- client/src/components/FiltersAndOrders/Filters/Filters.jsx
+++ client/src/components/FiltersAndOrders/Filters/Filters.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { filterbyOrigin } from '../../../redux/actions'
+import { useDispatch, useSelector } from 'react-redux'
+import { filterbyOrigin, filterbyTemps } from '../../../redux/actions'
 
 const Filters = () => {
+    const temperaments = useSelector(state => state.temperaments)
+
     const dispatch = useDispatch()
 
-    const handleFilter = (e) => {
+    const handleFilterByOrigin = (e) => {
         dispatch(filterbyOrigin(e.target.value))
+    }
+
+    const handleFilterByTemp = (e) => {
+        dispatch(filterbyTemps(e.target.value))
     }
 
     return (
         <div>
-            <select onChange={handleFilter}>
+            <select onChange={handleFilterByOrigin}>
                 <option disabled selected value="">API | DB</option>
                 <option value="API">API</option>
                 <option value="DB">DB</option>
             </select>
+
+            {
+                temperaments.length ? (
+                    <select onChange={handleFilterByTemp}>
+                        <option disabled selected value="">TEMPERAMENTS</option>
+                        <option value="ALL">ALL</option>
+                        {temperaments.map(temp => (
+                            <option key={temp.id} value={temp.name}>{temp.name}</option>
+                        ))}
+                    </select>
+                ) : null
+            }
         </div>
     )
 }
