e2eca4fd246be3fb821a8f1d31375ed9dd9de6fe
Matias Cancina <cancinamatias@gmail.com>
2023-07-12 18:36:21 -0300

    junto los filters y orders

---
+++ client/src/components/FiltersAndOrders/FiltersAndOrders.jsx
@@ -0,0 +1,23 @@
+import Orders from './Orders/Orders'
+import Filters from './Filters/Filters'
+import { useDispatch } from 'react-redux'
+import { resetFilters } from '../../redux/actions'
+import style from './FiltersAndOrders.module.css';
+
+const FiltersAndOrders = () => {
+    const dispatch = useDispatch();
+
+    const handleReset = () => {
+        dispatch(resetFilters())
+    }
+    
+  return (
+    <div className={style.filtersAndOrdersContainer}>
+        <Orders/>
+        <Filters/>
+        <button className={style.resetBtn} onChange={handleReset}>RESET</button>
+    </div>
+  )
+}
+
+export default FiltersAndOrders
