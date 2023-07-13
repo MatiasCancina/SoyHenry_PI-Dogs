0ec240fd9b08b6ff6d1af88dcebed10ad5644dc8
Matias Cancina <cancinamatias@gmail.com>
2023-07-12 23:33:12 -0300

    renderizado condicional para que no se muestre el pagination en el loading

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import style from './Pagination.module.css';
 
-const Pagination = ({ count, totalPages, nextHandler, prevHandler, firstPageHandler, lastPageHandler }) => {    //traigo todo lo que voy a usar del hook de Pagination 
+const Pagination = ({ dogs, count, totalPages, nextHandler, prevHandler, firstPageHandler, lastPageHandler }) => {    //traigo todo lo que voy a usar del hook de Pagination 
 
     return (
         <div className={style.paginationContainer}>
+            {dogs ? (
+                <>
+                    <div className={style.arrowsContainer}>
+                        <button className={style.arrow} onClick={prevHandler}>
+                            {"<"}
+                        </button>
 
-            <div className={style.arrowsContainer}>
-                <button className={style.arrow} onClick={prevHandler}>
-                    {"<"}
-                </button>
+                        <h3>{count} - {totalPages}</h3>
 
-                <h3>{count} - {totalPages}</h3>
+                        <button className={style.arrow} onClick={nextHandler}>
+                            {">"}
+                        </button>
 
-                <button className={style.arrow} onClick={nextHandler}>
-                    {">"}
-                </button>
+                    </div>
 
-            </div>
-
-            <button onClick={firstPageHandler} className={style.firstBtn}>
-                {"FIRST"}
-            </button>
-            <button onClick={lastPageHandler} className={style.lastBtn}>
-                {"LAST"}
-            </button>
+                    <button onClick={firstPageHandler} className={style.firstBtn}>
+                        {"FIRST"}
+                    </button>
+                    <button onClick={lastPageHandler} className={style.lastBtn}>
+                        {"LAST"}
+                    </button>
+                </>
+            )
+                : null}
         </div>
     )
 }
