5adb629c7f8dbcd39624a17807d602790e238f74
Matias Cancina <cancinamatias@gmail.com>
2023-07-14 19:28:12 -0300

    agrega boton delete db

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import style from './Card.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteDbDogs } from './deleteDbDog';
 
 export const Card = ({ dog }) => {  //? recibe dog por props de Cards
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const navigateHandler = () => {
         navigate(`/details/${dog.id}`);
@@ -17,6 +20,10 @@ export const Card = ({ dog }) => {  //? recibe dog por props de Cards
 
     return (
         <div className={style.cardContainer} key={dog.id}>
+            {isNaN(Number(dog.id)) &&
+                <button onClick={() => {deleteDbDogs(dog.id, dispatch)}}>x</button>
+            }
+
             <h1>{dog.name}</h1>
             <img onClick={navigateHandler} className={style.image} src={dog.image} alt={dog.name} />
             <div className={style.dogInfo}>
