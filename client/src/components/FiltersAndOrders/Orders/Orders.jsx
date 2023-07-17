2cbdab7f4119c592ac4a566f6e7b7d6d3b9096da
Matias Cancina <cancinamatias@gmail.com>
2023-07-17 18:31:53 -0300

    junta options

--- client/src/components/FiltersAndOrders/Orders/Orders.jsx
+++ client/src/components/FiltersAndOrders/Orders/Orders.jsx
@@ -4,30 +4,28 @@ import useFiltersState from "../../../utils/customHooks/useFiltersState";
 
 const Orders = () => {
     const { value, setValue } = useFiltersState()
-    const { name, weight, } = value
+    // const { name, weight, } = value
 
     const dispatch = useDispatch();
 
-    const handleOrderByName = (e) => {
-        dispatch(orderByName(e.target.value));
-        setValue({ ...value, name: e.target.value, weight: 'original' })
-    }
+    const handleOrders = (e) => {
+        if (e.target.value === "A-Z" || e.target.value === "Z-A") {
+            dispatch(orderByName(e.target.value));
+            setValue({ ...value, name: e.target.value, weight: 'original' })
+        }
 
-    const handleOrderByWeight = (e) => {
-        dispatch(orderByWeight(e.target.value));
-        setValue({ ...value, weight: e.target.value, name: 'original' })
+        if (e.target.value === "heavier" || e.target.value === "lighter") {
+            dispatch(orderByWeight(e.target.value));
+            setValue({ ...value, weight: e.target.value, name: 'original' })
+        }
     }
 
     return (
         <div>
-            <select value={name} onChange={handleOrderByName}>
-                <option disabled value="original">A-Z</option>
+            <select  onChange={handleOrders}>
+                <option disabled selected value="">ORDERS</option>
                 <option value="A-Z">A-Z</option>
                 <option value="Z-A">Z-A</option>
-            </select>
-
-            <select value={weight} onChange={handleOrderByWeight}>
-                <option disabled value="original">WEIGHT</option>
                 <option value="heavier">HEAVIER</option>
                 <option value="lighter">LIGHTER</option>
             </select>
