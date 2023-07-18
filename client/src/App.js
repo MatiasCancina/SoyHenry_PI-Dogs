ca5e3bd25186c8e49cebff265800aa4b5f31983b
Matias Cancina <cancinamatias@gmail.com>
2023-07-18 12:37:52 -0300

    agrega ErrorPage

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Details from './components/Details/Details';
 import NavBar from './components/NavBar/NavBar';
 import CreateDog from './components/CreateDog/CreateDog';
 import './App.css';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/details/:id' element={<Details />} />
         <Route path='/create' element={<CreateDog />} />
+        <Route path='*' element={<ErrorPage/>}/>
       </Routes>
     </div>
   );
---
+++ client/src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <img src='https://st.depositphotos.com/31068556/51146/v/600/depositphotos_511461970-stock-illustration-concept-design-of-an-empty.jpg' alt="" />
+    </div>
+  )
+}
+
+export default ErrorPage
