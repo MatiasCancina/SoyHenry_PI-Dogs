4446ef1df9de1e64408c0e39818ee683ffeb5804
Matias Cancina <cancinamatias@gmail.com>
2023-07-18 16:35:16 -0300

    orden alfabetico de los temps en el select

--- client/src/components/FiltersAndOrders/Filters/Filters.jsx
+++ client/src/components/FiltersAndOrders/Filters/Filters.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { filterbyOrigin, filterbyTemps } from '../../../redux/actions'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterbyOrigin, filterbyTemps } from '../../../redux/actions';
 
 const Filters = () => {
-    const temperaments = useSelector(state => state.temperaments)
+  const temperaments = useSelector((state) => state.temperaments);
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const handleFilterByOrigin = (e) => {
-        dispatch(filterbyOrigin(e.target.value))
-    }
+  const handleFilterByOrigin = (e) => {
+    dispatch(filterbyOrigin(e.target.value));
+  };
 
-    const handleFilterByTemp = (e) => {
-        dispatch(filterbyTemps(e.target.value))
-    }
+  const handleFilterByTemp = (e) => {
+    dispatch(filterbyTemps(e.target.value));
+  };
 
-    return (
-        <div>
-            {
-                temperaments.length ? (
-                    <select onChange={handleFilterByTemp}>
-                        <option disabled selected value="original">ALL TEMPERAMENTS</option>
-                        {temperaments.map(temp => (
-                            <option key={temp.id} value={temp.name}>{temp.name}</option>
-                        ))}
-                    </select>
-                ) : null
-            }
+  // Ordenar los temperamentos en orden alfabético por el atributo "name"
+  const sortedTemperaments = temperaments.sort((a, b) => a.name.localeCompare(b.name));
 
-            <select onChange={handleFilterByOrigin}>
-                <option disabled selected value="original">API | DB</option>
-                <option value="API">API</option>
-                <option value="DB">DB</option>
-            </select>
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* Select de temperamentos */}
+      {temperaments.length ? (
+        <select onChange={handleFilterByTemp}>
+          <option disabled selected value="original">
+            ALL TEMPERAMENTS
+          </option>
+          {sortedTemperaments.map((temp) => (
+            <option key={temp.id} value={temp.name}>
+              {temp.name}
+            </option>
+          ))}
+        </select>
+      ) : null}
 
-export default Filters
+      {/* Select de origen */}
+      <select onChange={handleFilterByOrigin}>
+        <option disabled selected value="original">
+          API | DB
+        </option>
+        <option value="API">API</option>
+        <option value="DB">DB</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filters;
