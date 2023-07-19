bdadaf0611cf0efed9ebfb410e3df29c568147d9
Matias Cancina <cancinamatias@gmail.com>
2023-07-19 09:29:12 -0300

    elimina console log

--- client/src/components/Card/deleteDbDog.js
+++ client/src/components/Card/deleteDbDog.js
@@ -4,9 +4,8 @@ import { deleteDbDogsAction } from "../../redux/actions";
 export const deleteDbDogs = async (id, dispatch) => {
     try {
         dispatch(deleteDbDogsAction(id))
-        const { data } = await axios.delete(`http://localhost:8080/dogs/${id}`)
+        await axios.delete(`http://localhost:8080/dogs/${id}`)
         alert('The dog has been deleted')
-        console.log(data);
     } catch (error) {
         console.log(error);
     }
