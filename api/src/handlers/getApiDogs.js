b2d930605513b06a0780cf5c21160b312a179086
Matias Cancina <cancinamatias@gmail.com>
2023-09-19 12:19:17 -0300

    fix image

--- api/src/handlers/getApiDogs.js
+++ api/src/handlers/getApiDogs.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const URL = 'https://api.thedogapi.com/v1/breeds'
 
 const getApiDogs = async () => {
-    const { data } = await axios(URL)
+    const { data } = await axios(`${URL}?api_key=${process.env.API_KEY}`)
     const infoApi = data.map(   //por cada obj (dog) se trae las props indicadas
         (dog) => {
             return {
